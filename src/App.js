4c3a63f26135a3aa2570744ddb87db4cabdbe3c0
Shaan3110 <hitarasigan0987654321@gmail.com>
2022-06-09 16:50:11 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
           Change2  
-=======
-          Change1
->>>>>>> testbranch1
         </p>
         <a
           className="App-link" 
