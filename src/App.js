c6acdd2cb15a111bc2a0522a63db9ba85158542b
Shaan3110 <hitarasigan0987654321@gmail.com>
2022-06-09 11:09:14 +0530

    conflictmerge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Change1  
+          Change2  
         </p>
         <a
           className="App-link" 
